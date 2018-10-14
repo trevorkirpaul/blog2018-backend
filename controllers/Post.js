dac474897140c86db23d4b248f07ea591791354f
Trevor Kirpaul <trevorkirpaul@gmail.com>
2018-10-14 13:39:25 -0400

    added reference to author model on posts and added population to fetch posts endpoint

--- controllers/Post.js
+++ controllers/Post.js
@@ -5,6 +5,7 @@ const create = (req, res) => Post.create(req.body.post)
   .catch(err => res.json({ error: err, hasError: true }));
 
 const getAllPosts = (req, res) => Post.find()
+  .populate('author')
   .then(posts => res.json({ success: true, posts }))
   .catch(err => res.json({ errpr: err, hasError: true }));
 
